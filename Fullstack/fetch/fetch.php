b18bce133018ea375f799ce7c23f057f066a4cee
the3rdchild <ytcrash555@gmail.com>
2025-01-11 13:59:32 +0700

    update frontend

--- Fullstack/fetch/fetch.php
+++ Fullstack/fetch/fetch.php
@@ -6,7 +6,6 @@ header('Content-Type: application/json');
 $db = Database::connect();
 
 try {
-    // Fetch the latest detection from the database
     $sql = "SELECT 
                 session_id, 
                 height_location, 
@@ -23,32 +22,25 @@ try {
     $stmt->execute();
     $latestDetection = $stmt->fetch();
 
-    // Access the image directory
     $imageDir = realpath(dirname(__FILE__) . '/../..' . '/Machine Learning/Result/image');
     
-    // Check if the directory exists
     if ($imageDir && is_dir($imageDir)) {
         $images = array_diff(scandir($imageDir), array('..', '.'));
         
-        // Sort images by file modification time
         usort($images, function ($a, $b) use ($imageDir) {
             return filemtime($imageDir . '/' . $b) - filemtime($imageDir . '/' . $a);
         });
         
-        // Get the latest image
         $latestImage = reset($images);
         $latestImagePath = $latestImage ? "Machine Learning/Result/image/" . $latestImage : 'No images found'; 
-        // D:/Program Files/xampp/htdocs/cancer-detection/Machine Learning/Result/image
-        } else {
+        // D:\Programs\XAMPP\htdocs\cancer_detection\Machine Learning\Result\image        } else {
         $latestImagePath = 'Error accessing image directory';
     }
 
-    // Generate a list of all images
     $imageList = array_map(function ($image) use ($imageDir) {
         return "http://localhost/cancer_detection/Machine%20Learning/Result/image/" . $image;
     }, array_diff(scandir($imageDir), array('..', '.')));
     
-    // Send JSON response
     $response = [
         'camera_feed' => 'http://127.0.0.1:4000/preview', 
         'latest_detection' => $latestDetection ? $latestDetection : 'No recent detection',
